e00ca73d5dd3459279873e97bc82c1c75bfe9569
Susan Marek <smarek@chaskamn.com>
2018-01-12 10:58:19 -0600

    progressing

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -19,39 +19,41 @@
 
       // Capture User Inputs and store them into variables
       var name = $("#name").val().trim();
-      var role = $("#role").val().trim();
-      var startDate = $("#startDate").val().trim();
-      var monthlyRate = $("#monthlyRate").val().trim();
+      var destination = $("#destination").val().trim();
+      var trainTime = $("#trainTime").val().trim();
+      var frequency = $("#frequency").val().trim();
+      var minutesAway = 45;
 
       // var today = new moment();
       // var emp_date = new moment(startDate);
       // var dateDiff = moment().subtract(startDate);
       // console.log(dateDiff);
-      var monthsWorked = Math.floor(moment().diff(startDate,"months",true));
-      console.log(monthsWorked);
+      // var monthsWorked = Math.floor(moment().diff(startDate,"months",true));
+      // console.log(monthsWorked);
 
-      var totalBilled = monthsWorked * monthlyRate;
+      // var totalBilled = monthsWorked * monthlyRate;
 
       console.log("name: " + name);
-      console.log("role: " + role);
-      console.log("start: " + startDate);
-      console.log("rate: " + monthlyRate);
-      console.log("months: " + monthsWorked);
-      console.log("total: " + totalBilled);
+      console.log("destination: " + destination);
+      console.log("time: " + trainTime);
+      console.log("freqeuncy: " + frequency);
+      // console.log("months: " + monthsWorked);
+      // console.log("total: " + totalBilled);
 
       database.ref().push({
         name: name,
-        role: role,
-        startDate: startDate,
-        monthlyRate: monthlyRate,
-        monthsWorked: monthsWorked,
-        totalBilled: totalBilled,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency,
+        minutesAway: minutesAway,
+        // monthsWorked: monthsWorked,
+        // totalBilled: totalBilled,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
       });
       $("#name").val("");
-      $("#role").val("");
-      $("#startDate").val("");
-      $("#monthlyRate").val("");
+      $("#destination").val("");
+      $("#frequency").val("");
+      $("#trainTime").val("");
     });
 
      database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
@@ -59,11 +61,11 @@
 
       var tr = $('<tr>'
           + '<td>' + snapshot.val().name + '</td>'
-          + '<td>' + snapshot.val().role + '</td>'
-          + '<td>' + snapshot.val().startDate + '</td>'
-          + '<td>' + snapshot.val().monthsWorked + '</td>'
-          + '<td>' + snapshot.val().monthlyRate + '</td>'
-          + '<td>' + snapshot.val().totalBilled + '</td></tr>'
+          + '<td>' + snapshot.val().destination + '</td>'
+          + '<td>' + snapshot.val().frequency + '</td>'
+          + '<td>' + snapshot.val().trainTime + '</td>'
+          + '<td>' + snapshot.val().minutesAway + '</td></tr>'
+          // + '<td>' + snapshot.val().totalBilled + '</td></tr>'
           );
       // Writes the saved value from firebase to our display
       $("#addRow").prepend(tr);
