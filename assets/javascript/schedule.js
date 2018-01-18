29b6a11e7accdbb8527a3aaa05adfb0e269470ec
Susan Marek <smarek@chaskamn.com>
2018-01-17 21:04:56 -0600

    finished

--- assets/javascript/schedule.js
+++ assets/javascript/schedule.js
@@ -15,12 +15,11 @@
 
     var database = firebase.database();
 
-
     // Define function to update time
     var update = function () {
         currentTime = moment();
         $("#time").text("Current Time " + (moment(currentTime).format("h:mm:ss A")));
-         console.log("db time: " + currentTime);
+         // console.log("db time: " + currentTime);
 
          // database.ref().push({
          // currentTime: currentTime
@@ -65,11 +64,9 @@
       $("#trainTime").val("");
     });
 
-
     database.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
       console.log(snapshot.val());
 
-      // var minutesAway = moment().diff(moment(snapshot.val().trainTime,"HH.mm"),"minutes");
       var minutesAway = moment(snapshot.val().trainTime,"HH.mm").diff(moment(),"minutes");
       console.log(minutesAway);
       console.log("------------");
@@ -106,19 +103,3 @@
     }, function(errorObject) {
       console.log("The read failed: " + errorObject.code);
     });
-
-
-
-    // Notes for time manipulation
-
-    // // Time apart (remainder)
-    // var tRemainder = diffTime % tFrequency;
-    // console.log(tRemainder);
-
-    // // Minute Until Train
-    // var tMinutesTillTrain = tFrequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-    // // Next Train
-    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
